44a35637f402fd7e828b0706b86ec99684f5bd31
Wellstone515 <info.fullmoons@gmail.com>
2019-06-25 19:21:20 +0900

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts
+  resources :users, only:[:index, :show, :edit, :update]
+  get 'users/:id/post', to: 'users#user_post'
+  root to: "posts#index"
 end
