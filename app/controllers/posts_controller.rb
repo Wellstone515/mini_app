80888d8f8acec0eb5975fc8d6911ae92e56e8acb
Wellstone515 <info.fullmoons@gmail.com>
2019-06-25 19:19:04 +0900

    Create controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @posts = Post.order(id: "desc")
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.create(post_params)
+    if post.save
+      redirect_to controller: :posts, action: :index
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    redirect_to controller: :posts, action: :index if @post.user_id != current_user.id
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params) if @post.user_id == current_user.id
+    if @post.save
+      redirect_to controller: :posts, action: :show
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy if post.user_id == current_user.id
+    redirect_to controller: :posts, action: :index
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body).merge(user_id: current_user.id)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    redirect_to controller: :posts, action: :index if @user.id != current_user.id
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    redirect_to controller: :posts, action: :index if @user.id != current_user.id
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params) if user.id == current_user.id
+    if user.save
+      redirect_to controller: :users, action: :show
+    else
+      render :edit
+    end
+  end
+
+
+
+  def user_post
+    @user = User.find(params[:id])
+    @posts = Post.where(user_id: "#{params[:id]}")
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
