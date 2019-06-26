fc5bb291d841dc576f21c53c814cdc54a9adb8bd
Wellstone515 <info.fullmoons@gmail.com>
2019-06-26 13:38:19 +0900

    Update users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
 
-  def show
-    @user = User.find(params[:id])
-    redirect_to controller: :posts, action: :index if @user.id != current_user.id
-  end
-
   def edit
     @user = User.find(params[:id])
     redirect_to controller: :posts, action: :index if @user.id != current_user.id
@@ -14,7 +9,7 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     user.update(user_params) if user.id == current_user.id
     if user.save
-      redirect_to controller: :users, action: :show
+      redirect_to controller: :posts, action: :index
     else
       render :edit
     end
@@ -24,7 +19,7 @@ class UsersController < ApplicationController
 
   def user_post
     @user = User.find(params[:id])
-    @posts = Post.where(user_id: "#{params[:id]}")
+    @posts = Post.where(user_id: "#{params[:id]}").order(id: "desc")
   end
 
   private
